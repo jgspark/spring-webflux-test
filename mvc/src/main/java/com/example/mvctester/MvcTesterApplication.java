84328df3fae65147e0f7cb0f2f602e41403f0fa5
spark <pnw123456@icloud.com>
2023-06-11 12:07:27 +0900

    build: rebuild to project

--- mvc/src/main/java/com/example/mvctester/MvcTesterApplication.java
+++ mvc/src/main/java/com/example/mvctester/MvcTesterApplication.java
@@ -1,13 +1,13 @@
-package com.example.producer;
+package com.example.mvctester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class MvcTesterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProducerApplication.class, args);
+        SpringApplication.run(MvcTesterApplication.class, args);
     }
 
 }
--- webflux/src/main/java/com/example/webfluxtester/WebfluxTesterApplication.java
+++ webflux/src/main/java/com/example/webfluxtester/WebfluxTesterApplication.java
@@ -1,13 +1,13 @@
-package com.example.consumer;
+package com.example.webfluxtester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsumerApplication {
+public class WebfluxTesterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        SpringApplication.run(WebfluxTesterApplication.class, args);
     }
 
 }
