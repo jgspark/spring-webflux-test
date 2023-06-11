f8d94e8e9bff680ee069fdbdc3ec128898a253ec
jgspark <pnw123456@icloud.com>
2023-06-11 18:52:18 +0900

    fix: ServerHttpRequest remove

--- webflux/src/main/java/com/example/webfluxtester/web/WebFluxController.java
+++ webflux/src/main/java/com/example/webfluxtester/web/WebFluxController.java
@@ -1,6 +1,5 @@
 package com.example.webfluxtester.web;
 
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,7 +10,7 @@ import java.time.Duration;
 public class WebFluxController {
 
     @GetMapping("/flux_result")
-    public Mono getResult(ServerHttpRequest request) {
+    public Mono getResult() {
         return Mono.defer(() -> Mono.just("Result is ready!"))
                 .delaySubscription(Duration.ofMillis(500));
     }
